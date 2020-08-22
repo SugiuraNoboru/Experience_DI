efcc1d308acf500e05656ec9b09a30c4ce125777
SugiuraNoboru <67428673+SugiuraNoboru@users.noreply.github.com>
2020-08-22 21:36:29 +0900

    Add files via upload

---
+++ HelloController.kt
@@ -0,0 +1,19 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController(private val greeter: Greeter) {
+
+    @GetMapping("hello")
+    fun hello(@RequestParam("name") name: String): String = greeter.hello(name)
+}
+//class HelloController {
+//
+//    @GetMapping("hello")
+//    fun hello(@RequestParam("name") name: String): String = "Hello, $name!"
+
+}
+
